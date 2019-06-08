8b19fe29397ca8225b24bf0f5837326f7f126914
Gerhard Niemand <gerhard.n@afrihost.com>
2019-06-08 17:54:37 +0200

    Moved over to a capserJS script to make the test more reliable also updated Readme

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Phantom JS script to do a speedtest on myBroadband
 
 This script is not very reliable but it works. Need to figure out a way to actually make JS wait properly.
 
+Requirements
+PhantomJS and CasperJS, also NPM (To install them)
+```
+npm install -g phantomjs casperjs
+```
+
 Usage.
 ```
-./phantomjs speedtest.js
+casperjs speedtest.js
 ```
--- speedtest.js
+++ speedtest.js
@@ -1,85 +1,50 @@
-"use strict";
+var casper = require('casper').create();
 
+var webpage = "http://speedtest.mybroadband.co.za";
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds) {
-            break;
-        }
-    }
-};
+casper.start(webpage);
+casper.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0");
+casper.options.waitTimeout = 600000;
 
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 50000, //< Default Max Timout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function () {
-            if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof (testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-            } else {
-                if (!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    //console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof (onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 250); //< repeat check every 250ms
-};
-
-
-var page = require('webpage').create();
-
-page.settings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0";
-
-// Open Twitter on 'sencha' profile and, onPageLoad, do...
-page.open("http://speedtest.mybroadband.co.za/", function (status) {
-    // Check for page load success
-    if (status !== "success") {
-        console.log("Unable to access network");
-    } else {
-        console.log("Waiting a bit before doing anything");
-        sleep(5000);
-        // Wait for 'start' to be visible
-        waitFor(function () {
-            // Check in the page if a specific element is now visible
-            return page.evaluate(function () {
-                return $("#start").is(":visible");
-            });
-        }, function () {
-            console.log("The sign-in dialog should be visible now.");
-            console.log("Clicking start test");
-            page.evaluate(function () {
-                $("#start").click()
-            });
-            sleep(5000);
-            waitFor(function () {
-                return page.evaluate(function () {
-                    return $("#start").is(":visible");
-                })
-            }, function () {
-                console.log("Test is done");
-                var ping = page.evaluate(function () {
-                    return $("#ping").children(":visible").text();
-                });
-                console.log(ping);
-                var download = page.evaluate(function () {
-                    return $("#download").children(":visible").text();
-                });
-                console.log(download);
-                var upload = page.evaluate(function () {
-                    return $("#upload").children(":visible").text();
-                });
-                console.log(upload);
-                page.render('speedtest.png');
-                phantom.exit();
-            });
-        });
-    }
+casper.then(function (){
+    console.log("Waiting a bit before doing anything");
+    casper.wait(2000, function () {
+        console.log("Done waiting");
+    })
 });
+
+casper.then(function () {
+   console.log("Clicking the button");
+   casper.waitUntilVisible("#start", function (){
+    casper.click("#start");
+    casper.wait(10000, function() {
+        console.log("Waiting for the test to complete");
+    })
+   });
+});
+
+casper.then(function() {
+   casper.waitUntilVisible("#start", function() {
+     console.log("Looks like the test is done. Extracting results");
+   });
+});
+
+casper.then(function() {
+    var mbpsRegex = /\S+(?=Mbps)/;
+    var msRegex = /\S+(?=ms)/;
+
+    var downloadString = casper.getElementInfo("#download").text;
+    var uploadString = casper.getElementInfo("#upload").text;
+    var pingString = casper.getElementInfo("#ping").text;
+
+    var download = mbpsRegex.exec(downloadString);
+    var upload = mbpsRegex.exec(uploadString);
+    var ping = msRegex.exec(pingString);
+
+    console.log("Download Speed: " + download + "Mbps");
+    console.log("Upload Speed: " + upload + "Mbps");
+    console.log("Ping: " + ping + "ms");
+    console.log("Done");
+});
+
+casper.run();
